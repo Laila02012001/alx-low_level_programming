b97cdfd2e07651249a33a4ecd2e4b31f5975d365
laila02012001 <lailaelmakhloufi0201@gmail.com>
2023-07-16 14:17:35 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int l;
+
+	for (l = '0'; l <= '9'; l++)
+		putchar(l);
+	for (l = 'a'; l < 'g'; l++)
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
