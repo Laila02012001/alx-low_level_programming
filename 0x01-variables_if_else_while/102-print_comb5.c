23b3c1c6d6783a8042880c93c745c477aa5dcd04
laila02012001 <lailaelmakhloufi0201@gmail.com>
2023-07-17 07:07:30 -0700

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,32 +7,32 @@
  */
 int main(void)
 {
-	int a, b, c, d;
+int a, b, c, d;
 
-	for (a = '0'; a <= '9'; a++)
+for (a = '0'; a <= '9'; a++)
+{
+	for (b = '0'; b <= '9'; b++)
 	{
-		for (b = '0'; b <= '9'; b++)
+		for (c = '0'; c <= '9'; c++)
 		{
-			for (c = '0'; c <= '9'; c++)
+			for (d = '0'; d <= '9'; d++)
 			{
-				for (d = '0'; d <= '9'; d++)
+				if (a + b < c + d)
 				{
-					if (a + b < c + d)
-					{
-						putchar(a);
-						putchar(b);
-						putchar(' ');
-						putchar(c);
-						putchar(d);
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
 					if (a == '9' && b == '8' && c == '9' && d == '9')
 						break;
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
 	}
-	putchar('\n');
-	return (0);
+}
+putchar('\n');
+return (0);
 }
