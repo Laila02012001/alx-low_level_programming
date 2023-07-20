fb250a3d6292ec74e14a637d4d5d648af5340c6b
laila02012001 <lailaelmakhloufi0201@gmail.com>
2023-07-20 04:32:50 -0700

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - print the numbers
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	int l;
+
+	for (l = '0'; l <= '9'; l++)
+		_putchar(l);
+	_putchar('\n');
+}
