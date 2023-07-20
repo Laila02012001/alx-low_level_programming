32363adfe6359135ebd32499876ff313a55fbaef
laila02012001 <lailaelmakhloufi0201@gmail.com>
2023-07-20 13:36:52 -0700

    12. Numbers have life; they're not just symbols on paper

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,28 +1,21 @@
 #include "main.h"
 /**
- * print_number - Print number
+ * print_number -  print an integer.
  * @n: integer
  * Return: Nothing
  */
 void print_number(int n)
 {
+	unsigned int laila = n;
+
 	if (n < 0)
 	{
+		laila = -laila;
 		_putchar('-');
-		n = -n;
 	}
-
-	int laila = 1;
-
-	while (n / laila >= 10)
+	if (laila > 9)
 	{
-		laila *= 10;
+		print_number(laila / 10);
 	}
-
-	while (laila != 0)
-	{
-		_putchar(n / laila + '0');
-		n %= laila;
-		laila /= 10;
-	}
+	_putchar(laila % 10 + '0');
 }
