25b2535583adad289e2bbacc3e909929b2945f21
laila02012001 <lailaelmakhloufi0201@gmail.com>
2023-07-24 07:45:06 -0700

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - print a string, followed by a new line, to stdout
+ * @str: char
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
