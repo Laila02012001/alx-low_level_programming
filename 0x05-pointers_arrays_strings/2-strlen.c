dee15d0636747b3583bf91c14b5f3e6128c9df2a
laila02012001 <lailaelmakhloufi0201@gmail.com>
2023-07-24 07:40:06 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - return the length of a string
+ * @s: integer
+ * Return: return lenght of a string
+ */
+int _strlen(char *s)
+{
+	int l;
+
+	l = strlen(s);
+	return (l);
+}
