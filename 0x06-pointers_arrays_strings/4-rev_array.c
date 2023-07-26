3cf897815066912c027a98d936be3f8e4dd59819
laila02012001 <lailaelmakhloufi0201@gmail.com>
2023-07-26 15:46:36 -0700

    4. I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the content of an array of integers
+ * @a: var
+ * @n: var
+ *
+ * Return : string
+ */
+void reverse_array(int *a, int n)
+{
+	int l, o, laila;
+
+	for (l = 0; l < n; l++)
+	{
+		for (o = l + 1; l < n; l++)
+		{
+			laila = *(a + l);
+			*(a + l) = *(a + o);
+			*(a + o) = laila;
+
+		}
+	}
+}
