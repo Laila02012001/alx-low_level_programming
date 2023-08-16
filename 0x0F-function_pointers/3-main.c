ab625f41f80cd273106cc09ca893ee9d667ff890
laila02012001 <lailaelmakhloufi0201@gmail.com>
2023-08-16 14:36:29 -0700

    lil

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,8 +9,9 @@
  */
 int main(int argc, char *argv[])
 {
-	int l, o;
-	int (*laila)(int, int);
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
 
 	if (argc != 4)
 	{
@@ -18,23 +19,28 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (argv[2][1])
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	laila = get_op_func(argv[2]);
+	o = *argv[2];
 
-	if (laila == NULL)
+	if ((o == '/' || o == '%') && arg2 == 0)
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
 
-	l = atoi(argv[1]);
-	o = atoi(argv[3]);
+	result = func(arg1, arg2);
 
-	printf("%d\n", laila(l, o));
+	printf("%d\n", result);
+
 	return (0);
 }
