73b596d8cf8f228bb65b9733b39b33419974230d
Laila02012001 <lailaelmakhloufi0201@gmail.com>
2023-08-29 16:26:48 -0700

    lola

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,1 +1,40 @@
-ii
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a linked list
+ * @h: pointer
+ *
+ * Return: number of elements in the list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t laila = 0;
+	int lola;
+	listint_t *lol;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		lola = *h - (*h)->next;
+		if (lola > 0)
+		{
+			lol = (*h)->next;
+			free(*h);
+			*h = lol;
+			laila++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			laila++;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (laila);
+}
